9214b338093a880ad812bf3a973a9504bc864117
Justyna <justyna.mulewska@gmail.com>
2018-09-08 22:45:47 +0200

    Simple template usage added.

--- flask_app.py
+++ flask_app.py
@@ -2,10 +2,14 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask 2!'
+    return render_template('main.html', report_weeks={'36': ['Groceries 🍞🧀',20], '37': ['Dining Out', 35]})
+    #return 'Hello from Flask 2!'
 
+if __name__ == '__main__':
+    app.run(debug=True)
